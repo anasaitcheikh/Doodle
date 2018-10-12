a5d048ef2c17a2792db3378e89284488686787d9
mohamed <mohamed.adegbindin@etu.u-pec.fr>
2018-10-12 19:10:42 +0200

    developpement des collections

--- backend/src/model/connection.js
+++ backend/src/model/connection.js
@@ -22,20 +22,45 @@ mongoose.connect(hostname, { useNewUrlParser: true }, function(err) {
 })
 
 
-// Création du schéma pour les commentaires
-var RenionSchema = new mongoose.Schema({
-  // pseudo : { type : String, match: /^[a-zA-Z0-9-_]+$/ },
-  // contenu : String,
-  // date : { type : Date, default : Date.now }
+const DateRenion = new mongoose.Schema({
+                    _id : { type : mongoose.Schema.Types.ObjectId },
+                    date : { type: Date, default: Date.now },
+                    hourStart : { type : String },
+                    hourEnd : { type : String }
+                 })  
+
+const Comment = new mongoose.Schema({
+                  _id : { type : mongoose.Schema.Types.ObjectId },
+                  name :  { type : String },
+                  email : { type : String },
+                  text : { type : String },
+                  create_at : { type: Date, default: Date.now },
+                  update_at : { type: Date, default: Date.now }
+                })
+
+
+const Participant = new mongoose.Schema({
+                      _id : { type : mongoose.Schema.Types.ObjectId },
+                      name :  { type : String },
+                      email : { type : String },
+                      create_at : { type: Date, default: Date.now },
+                      update_at : { type: Date, default: Date.now }
+                    })
+
+
+// Création du schéma pour les renions
+const RenionSchema = new mongoose.Schema({
   title : { type : String },
   place : { type : String },
   note : { type : String },
-  //date : {},
+  date : [DateRenion],
   addComment : { type : Boolean },
   maxParticipant : { type: Number, min: 1 },
-  //comment : {},
-  //admin : Schema.Types.ObjectId,
-  //participant : Schema.Types.ObjectId,
+  comment : [Comment],
+  admin : Participant,
+  participant : [Participant],
+  create_at : { type: Date, default: Date.now },
+  update_at : { type: Date, default: Date.now }
 });
  
 
@@ -46,9 +71,37 @@ var RenionModel = mongoose.model('Renion', RenionSchema);
 // On crée une instance du Model
 // var renion = new RenionModel({ pseudo : 'Atinux' });
 // renion.contenu = 'Salut, super article sur Mongoose !';
-var renion = new RenionModel({ title : 'renion test' });
-renion.date = renion;
- 
+var renion = new RenionModel();
+renion.title = 'renion test 2.0'
+renion.place = 'Skype'
+renion.note = 'rdv pour la révision'
+renion.addComment = true
+renion.maxParticipant = 5
+renion.date.push({  
+                    date: new Date().now,
+                    hourStart : "begin",
+                    hourEnd : 'end'
+                  })
+renion.comment.push({
+                      name : 'moub name',
+                      email : 'moub email',
+                      text : 'moub text',
+                      create_at : new Date().now,
+                      update_at : new Date().now
+                    }) 
+renion.admin = { 
+                 name :'moub name admin',
+                 email : 'moub email admin',
+                 create_at : new Date().now,
+                 update_at : new Date().now
+                }
+renion.participant.push({
+                          name : 'moub participant',
+                          email : 'moub email',
+                          create_at : new Date().now,
+                          update_at : new Date().now
+                        })
+
 
 // On le sauvegarde dans MongoDB !
 renion.save(function (err) {
