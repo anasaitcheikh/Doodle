559a25c5026290b21de66dcfa0e9ecaf54f7f9fb
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2018-10-17 19:39:18 +0200

    feat(api): handle some routes
    Handle events, participants and messages routes

--- backend/src/api.js
+++ backend/src/api.js
@@ -4,18 +4,78 @@ const router = express.Router()
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now());
-    var now = new Date()
-    console.log( "Time "+now.getHours()+"h "+now.getMinutes());
     next();
 });
-/**
- * @résumé La route representant la demande de l'acceuil du site.
- * @param {string} route - La route vers la ressource recherchée. 
- * @param {function} callback - La fonction appelée à la fin du traitement, elle prends en paramètre l'objet requêtte et l'objet reponse.
-*/
-router.get('/', function(req, res) {
-    res.status(200).send("Hello world")
+
+//events
+router.get('/events', function(req, res) {
+    res.status('404').send("Not found")
+    res.end()
 });
+
+router.get('/events/:event_id', function(req, res) {
+    res.status('404').send("Not found")
+    res.end()
+});
+
+router.post('events', function(req, res) {
+    res.status('404').send("Not found")
+    res.end()
+});
+
+router.put('/events/:event_id', function(req, res) {
+    res.status('404').send("Not found")
+    res.end()
+});
+
+router.delete('/events/:id', function(req, res) {
+    res.status('404').send("Not found")
+    res.end()
+});
+
+//participants
+router.get('/events/:event_id/participants', (req, res) => {
+
+})
+
+router.get('/events/:event_id/participants/:id_participant', (req, res) => {
+
+})
+
+router.post('/events/:event_id/participants', (req, res) => {
+    
+})
+
+router.put('/events/:event_id/participants/:id_participant', (req, res) => {
+
+})
+
+router.delete('/events/:event_id/participants/:id_participant', (req, res) => {
+    
+})
+
+//comments
+router.get('/events/:event_id/comments', (req, res) => {
+
+})
+
+router.get('/events/:event_id/comments/:id_comment', (req, res) => {
+
+})
+
+router.post('/events/:event_id/comment', (req, res) => {
+    
+})
+
+router.put('/events/:event_id/comments/:id_comment', (req, res) => {
+
+})
+
+router.delete('/events/:event_id/comments/:id_comment', (req, res) => {
+    
+})
+
+
 /**
  * @résumé Receuille l'ensemble des demandes de ressources non disponible sur le serveur, 
  *         les traite et renvoie une page d'erreur à l'utilisateur pour l'informer de la non disponibilité de cette ressource.   
@@ -26,5 +86,6 @@ router.get('*', function(req, res) {
     res.status('404').send("Not found")
     res.end()
 });
+
   
 module.exports = router;
