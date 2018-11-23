a69909fb1e03221bbfdc4e1dd5928e4ceefc8bca
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2018-11-23 21:36:58 +0100

    feat(bodyparser): disable body parsing

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,8 +22,8 @@ const api = require('./api')
 
 const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.json())
 
 
 /**
