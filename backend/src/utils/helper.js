1f032e603ee8e672009b5c8c674395bfed1f4d34
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2018-11-23 21:35:51 +0100

    feat(helper): add error and status messages

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -8,6 +8,22 @@ isJSON = function(data){
     }
 }
 
+requestErrorMsg = {
+    jsonMalformed: "Malformed Request's JSON",
+    reunionNotFound: 'Reunion not found',
+    participantNotFound: 'Participant not found',
+    commentNotFound: 'Comment not found',
+    internalError: 'Internal Error'
+}
+
+responseStatus = {
+    error: 'error',
+    fail: 'fail',
+    succes: 'succes'
+}
+
 module.exports = {
-    isJson: isJSON
+    isJson: isJSON,
+    requestErrorMsg: requestErrorMsg,
+    responseStatus: responseStatus
 }
