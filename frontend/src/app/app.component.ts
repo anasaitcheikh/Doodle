c4688057e7623cf5bcfbf894936ecde6d9c342f6
Doussou Doumbia <doussou.doumbia@etu.u-pec.fr>
2018-12-14 13:42:12 +0100

    getting event name add component for login and signin

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Doodle';
   doodle = {
     titre:"ff"
+  }
+
+  ngOnInit(){
+
+  }
+
+  EventTitle = {
+      title: ""
+  }
+
+  addEvent(){
+     console.log("new event title: "+ this.EventTitle.title);
+     this.EventTitle.title = '';
   }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule }  from '@angular/forms';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
