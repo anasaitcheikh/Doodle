c3b08d392f11a66b69dda54990e53b6c64061a89
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2018-12-28 09:41:19 +0100

    feat(users-services): update

--- frontend/src/app/users.service.ts
+++ frontend/src/app/users.service.ts
@@ -1,39 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from "@angular/common/http";
-import {map} from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-   addUser = false;
-  createAccount(user){
-
-    this.http.post('http://localhost:8080/api/close/users',
+  createAccount(user) {
+    return this.http.post('http://localhost:8080/api/close/users',
       {
-         data : {
-            user: {
-              email: user.email,
-              password: user.password,
-              name: user.name
-
-            }
-         }
+        data: {
+          user: user
+        }
       }
-      ).subscribe(
-      data => {
-        //console.log("create account success ");
-        console.log(data);
-        this.addUser = true;
-      },
-      error => {
-        console.log("Error", error);
-      }
-    );
-    return this.addUser;
+    )
   }
 
 }
