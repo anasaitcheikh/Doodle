d7a79af2c730b5c3355399c3bb96903dee46ccb8
El Hadji Maguette KONATE <el.m.konate@gmail.com>
2019-01-06 00:25:34 +0100

    feat(redirect): component for simulating redirection

---
+++ frontend/src/app/redirect/redirect.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import  { ActivatedRoute, Router } from '@angular/router'
+
+@Component({
+  selector: 'app-redirect',
+  templateUrl: './redirect.component.html',
+  styleUrls: ['./redirect.component.css']
+})
+export class RedirectComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit() {
+    const finalPath = this.route.snapshot.paramMap.get('final_path')
+    console.log('redirectTo', finalPath)
+    this.router.navigate([finalPath.replace('--', '/')])
+  }
+
+}
